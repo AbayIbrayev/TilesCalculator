c29232ecb0de2dde33a45afcae6f5dca14f803e2
Abay <abayibrayev7@gmail.com>
2020-03-23 16:26:32 +0000

    tile calculator update

--- main.js
+++ main.js
@@ -3,23 +3,36 @@ let tilesAmount = document.querySelector('#tilesA'),
   tilesL = document.querySelector('#tilesL'),
   tilesBtn = document.querySelector('.tiles__btn'),
   tilesCheckbox = document.querySelector('#tiles-checkbox'),
+  tilesBlock = document.querySelector('.tiles__block'),
   tilesSurface = document.querySelector('#tiles-surface');
+
+// tilesW.addEventListener('change', () => {
+//   calcTilesSurface();
+// });
+// tilesL.addEventListener('change', () => {
+//   calcTilesSurface();
+// });
+// tilesAmount.addEventListener('change', () => {
+//   calcTilesSurface();
+// });
+tilesBlock.addEventListener('change', () => {
+  calcTilesSurface();
+});
+tilesCheckbox.addEventListener('change', () => {
+  calcTilesSurface();
+});
 
 function calcTilesSurface() {
   tilesSurface.innerHTML = '';
   if (tilesAmount.value != '' && tilesW.value != '' && tilesL.value != '' && tilesAmount.value > 0 && tilesW.value > 0 && tilesL.value > 0) {
     let tilesArea = parseFloat(tilesW.value) * parseFloat(tilesL.value);
     let result;
-    if (tilesCheckbox.checked == false) {
-      result = parseFloat(tilesAmount.value) * tilesArea;
-    } else {
+    if (tilesCheckbox.checked == true) {
       tilesArea = tilesArea + tilesArea * 0.1;
-      result = parseFloat(tilesAmount.value) * tilesArea;
     }
-    tilesSurface.innerHTML = `You will need <strong>${result.toFixed(2)} &#13217;</strong> of tiles`;
-  } else if (tilesAmount.value == 0 || tilesW.value == 0 || tilesL.value == 0) {
-    tilesSurface.innerText = 'Change the provided details!';
+    result = tilesArea / parseFloat(tilesAmount.value);
+    tilesSurface.innerHTML = `You will need <strong>${result.toFixed(2)}(${Math.round(result)})</strong> tiles or <strong>${tilesArea.toFixed(2)}(${Math.round(tilesArea)})&#13217;</strong>`;
   } else {
-    tilesSurface.innerText = 'All the inputs must be filled!';
+    tilesSurface.innerText = 'Change the provided details!';
   }
 }
